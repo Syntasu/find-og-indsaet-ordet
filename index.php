de67b3c79bf9bd6bb92c374eaa9a81007bb70844
Syntasu <administrator@syntasu.me>
2015-02-05 11:52:15 +0100

    Added project files.

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="js/opgave.js"></script>
+
+    <title>Find og indsaet ordet</title>
+<head>
+
+<body>
+    <div id="reset_wrapper"">
+        <img id="reset_image" src="img/button.png" width="40" height="36" />
+        <h3 id="reset_text">Igen</h3>
+    </div>
+
+    <div id="score_wrapper">
+        <p id="score_text" >Rigtige:&nbsp; <span id="score_correct">0</span> &nbsp; Forkerte: &nbsp; <span id="score_wrong">0</span></p>
+    </div>
+
+
+    <div id="content_wrapper">
+        <div id="situation">
+            <p id="situation_text">Situation text</p>
+        </div>
+
+        <div id="section1" class="section">
+            <h4 id="caption1" class="caption"> This is caption one:</h4>
+            <p id="question1" class="question">- Question1 question1<span class="dropable" id="drop1">drop answer here</span>bla bla bla</p><br/>
+            <p id="question2" class="question">- Question2 question2<span class="dropable" id="drop2">drop answer here</span>bla bla bla</p><br/>
+            <p id="question3" class="question">- Question3 question3<span class="dropable" id="drop3">drop answer here</span>bla bla bla</p><br/>
+        </div>
+
+        <div id="section2" class="section">
+            <h4 id="caption2" class="caption"> This is caption two:</h4>
+            <p id="question1" class="question">- Question1 Question1<span class="dropable" id="drop1">drop answer here</span>bla bla bla</p><br/>
+            <p id="question2" class="question">- Question2 question2<span class="dropable" id="drop2">drop answer here</span>bla bla bla</p><br/>
+            <p id="question3" class="question">- Question3 question3<span class="dropable" id="drop3">drop answer here</span>bla bla bla</p><br/>
+        </div>
+    </div>
+
+</body>
+
+
+</html>
