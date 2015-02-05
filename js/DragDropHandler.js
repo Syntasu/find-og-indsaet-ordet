874be5d45ee03852229d9ee7a5edffb034a6537a
Syntasu <administrator@syntasu.me>
2015-02-05 15:16:19 +0100

    Added comments to the drag drop JS.

--- js/DragDropHandler.js
+++ js/DragDropHandler.js
@@ -1,17 +1,28 @@
-var dragging = "";
-var dropped = "";
+var dragging = ""; // COntains element that is last dragged.
 
-function allowDrop(ev) {
+/* Check if its allowed to drop, prevent default behaviour.*/
+function allowDrop(ev)
+{
     ev.preventDefault();
 }
 
-function drag(ev) {
+/* Called when a draggable object is being dragged.
+Set the global variable to indentify the dragged element/object
+ */
+function drag(ev)
+{
     dragging = ev.target.innerHTML;
     ev.dataTransfer.setData('Text/html', ev.target.id);
 }
 
-function drop(ev, target) {
-    ev.preventDefault();
-    dropped = ev.target.innerHTML;
 
+/*Called when a element is dropped on this element.
+Check if answer is correct or wrong, perform action based off of that.*/
+function drop(ev, target)
+{
+    ev.preventDefault();
+    var dropped = ev.target.innerHTML;
+
+    target.innerHTML = dragging;
+    target.style.color = "black";
 }
