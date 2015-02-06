984bd86f917e412233ac949699a72e04af96be1a
Syntasu <administrator@syntasu.me>
2015-02-06 08:47:05 +0100

    Added GenerationOptions PHP file.

---
+++ php/GenerateOptions.php
@@ -0,0 +1,36 @@
+<?php
+
+function StartGeneratingOptions()
+{
+    $path = $_SERVER["DOCUMENT_ROOT"] . "/stageopdrachten/find-og-indsaet-ordet/json/options.json";
+    $jsonraw = GetJsonFileContent($path);
+
+    $results = json_decode($jsonraw, true);
+    foreach($results as $result)
+    {
+        foreach($result as $option)
+        {
+            CreateOption($option['id'], ToUTF8($option['word']));
+        }
+    }
+}
+
+function GetJsonFileContent($p)
+{
+    return file_get_contents($p);
+}
+
+function CreateOption($id, $text)
+{
+    echo '<div id="option'. $id . '" class="option">
+            <img id="option_image" src="img/button.png" width="30" height="26" />
+            <p draggable="true" ondragstart="drag(event)" id="option_text">'.$text.'</p>
+        </div>';
+}
+
+function ToUTF8($string){
+    return utf8_decode($string);
+}
+
+
+?>
